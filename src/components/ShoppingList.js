6dd093cdc0140c171843bb1eae27259187a6f3a3
Zettocs <valentin.scottez@outlook.fr>
2023-04-06 21:54:41 +0200

    Ajout de l'utilisation de l'API pour la page de gestion de stock

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,51 +1,49 @@
-import { shoesList } from "../datas/shoesList"
-import '../css/ShoppingList.css'
-
-
+import { useState, useEffect } from 'react';
+import '../css/ShoppingList.css';
 
 function Shoe(props) {
   return (
     <div className="produit-wrapper">
-
-
-				<div className="test">
-					<img className="chaussures"></img>
-				</div>
-				<h2 className="nom">{props.name}</h2>
-
-				<p className="reference">Référence :
-					{ props.référence }</p>
-
-				<p className="prix">{ props.prix }€</p>
-
-				<button className="afficher-article">
-					
-				</button>
-
-
-			</div>
-
-
-
-
-     
-      
-    
+      <div className="test">
+        <img className="chaussures">{props.Photo}</img>
+      </div>
+      <h2 className="nom">{props.name}</h2>
+      <p className="reference">Référence : {props.reference}</p>
+      <p className="prix">{props.prix}€</p>
+      <button className="afficher-article"></button>
+    </div>
   );
 }
 
 function ShoppingList() {
-  const shoeComponents = shoesList.map((shoe) => (
+  const [produits, setShoeData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/api/produits')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setShoeData(data);
+      })
+      .catch(error => console.error('Error fetching products:', error));
+  }, []);
+
+  const shoeComponents = produits.map((produits) => (
     <Shoe
-      key={shoe.id}
-      id={shoe.id}
-      name={shoe.name}
-      price={shoe.prix}
-      // Add more properties as needed
+      key={produits.IdProduit_}
+      id={produits.IdProduit_}
+      name={produits.Nom}
+      prix={produits.Prix}
+      reference={produits.Reference}
+      photo={produits.Photo}
     />
   ));
 
   return <div className="shoes-list">{shoeComponents}</div>;
 }
 
-export default ShoppingList
+export default ShoppingList;
